c413421f2485b7b7b2cdcdee006ebe293965d843
evolve6265-crypto <evolve6265@gmail.com>
2026-05-04 21:39:15 +0530

    Initialize Discord bot with command and event loaders

---
+++ index.js
@@ -0,0 +1,62 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Player } = require('discord-player');
+const { DefaultExtractors } = require('@discord-player/extractor');
+const fs = require('fs');
+const { initDB } = require('./utils/db');
+
+// Anti-Crash System
+process.on('unhandledRejection', (reason, p) => {
+    console.log(' [antiCrash] :: Unhandled Rejection/Catch');
+    console.log(reason, p);
+});
+process.on('uncaughtException', (err, origin) => {
+    console.log(' [antiCrash] :: Uncaught Exception/Catch');
+    console.log(err, origin);
+});
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages
+    ]
+});
+
+client.commands = new Collection();
+
+// Initialize Discord Player
+const player = new Player(client);
+player.extractors.loadMulti(DefaultExtractors);
+
+// Command Loader
+const loadCommands = () => {
+    const commandFolders = fs.readdirSync('./commands');
+    for (const folder of commandFolders) {
+        const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+        for (const file of commandFiles) {
+            const command = require(`./commands/${folder}/${file}`);
+            client.commands.set(command.data.name, command);
+        }
+    }
+};
+
+// Event Loader
+const loadEvents = () => {
+    const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+    for (const file of eventFiles) {
+        const event = require(`./events/${file}`);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args, client));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args, client));
+        }
+    }
+};
+
+loadCommands();
+loadEvents();
+initDB();
+
+client.login(process.env.
+             TOKEN);
